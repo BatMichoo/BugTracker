835aff99f4445145ad1a36139b671a510f60f58f
Miroslav <miroslavsotirov1@gmail.com>
2025-05-29 16:33:59 +0300

    Fixed SQL Identity jumping by 1000 due to cache

---
+++ Infrastructure/Migrations/20250529132333_RemoveIdentityCache.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveIdentityCache : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "ALTER DATABASE SCOPED CONFIGURATION SET IDENTITY_CACHE = OFF;",
+                suppressTransaction: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                sql: "ALTER DATABASE SCOPED CONFIGURATION SET IDENTITY_CACHE = ON;",
+                suppressTransaction: true);
+        }
+    }
+}
